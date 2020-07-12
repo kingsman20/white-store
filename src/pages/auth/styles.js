80e7ca761ffc47a85b5ef8220132bca1a8e054ef
Emmanuel Adesile <emmanueladesile@Emmanuels-MacBook-Pro.local>
2020-07-12 16:24:50 +0100

    design responsive sign in screen

--- src/pages/auth/styles.js
+++ src/pages/auth/styles.js
@@ -3,18 +3,63 @@ import { colors, gradient } from "../../styles/theme";
 
 const Container = styled.div`
   display: grid;
+
   grid-template-columns: 1fr 1fr;
   height: 100vh;
   font-size: 14px;
+
+  @media (max-width: 375px) {
+    grid-template-columns: 1fr;
+    background: ${colors.primary};
+  }
 `;
 
 const Title = styled.h1`
   font-size: 2rem;
   margin-bottom: 11px;
+
+  @media (max-width: 375px) {
+    display: none;
+  }
+`;
+
+const SmallScreenTopTitles = styled.div`
+  display: none;
+
+  @media (max-width: 375px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 365px;
+    color: ${colors.white};
+
+    & > * {
+      margin: 0;
+    }
+
+    .logo {
+      width: 50px;
+      height: 50px;
+    }
+
+    .title {
+      font-size: 2rem;
+    }
+
+    .smallText {
+      font-size: 1rem;
+      justify-self: flex-start;
+    }
+  }
 `;
 
 const SubText = styled.p`
   color: ${colors.primaryLight};
+
+  @media (max-width: 375px) {
+    display: none;
+  }
 `;
 
 const Wrapper = styled.div`
@@ -24,12 +69,37 @@ const Wrapper = styled.div`
   border-top-right-radius: 10px;
   border-bottom-right-radius: 10px;
   background: #fff;
+
+  @media (max-width: 375px) {
+    border-radius: 0;
+    padding: 0;
+    grid-template-columns: 1fr;
+    background: ${colors.primary};
+  }
+`;
+
+const FormWrapper = styled.div`
+  @media (max-width: 375px) {
+    border-top-right-radius: 16px;
+    border-top-left-radius: 16px;
+    border: 1px solid ${colors.white};
+    padding: 0 1rem;
+    grid-template-columns: 1fr;
+    background: ${colors.white};
+    height: calc(100vh - 365px);
+  }
 `;
 
 const StyledForm = styled.form`
   width: 365px;
   margin-top: 4.5rem;
   margin-bottom: 115px;
+
+  @media (max-width: 375px) {
+    margin-top: 1.3rem;
+    width: 325px;
+    margin-bottom: 1rem;
+  }
 `;
 
 const GradientBackground = styled.div`
@@ -40,10 +110,18 @@ const GradientBackground = styled.div`
     height: 100%;
     max-width: 100%;
   }
+
+  @media (max-width: 375px) {
+    display: none;
+  }
 `;
 
 const FormGroup = styled.div`
   margin-top: 2.5rem;
+
+  .fullname {
+    display: none;
+  }
 
   label {
     color: ${colors.primaryLight};
@@ -70,6 +148,14 @@ const FormGroup = styled.div`
     position: absolute;
     top: 15px;
     right: 0;
+  }
+
+  @media(max-width: 375px) {
+  margin-top: 1.5rem;
+
+    .fullname {
+      display: block;
+    }
   }
 `;
 
@@ -104,6 +190,12 @@ const BottomFormGroup = styled.div`
     display: flex;
     justify-content: space-between;
   }
+
+  @media (max-width: 375px) {
+    .top {
+      display: none;
+    }
+  }
 `;
 
 const Button = styled.div`
@@ -121,6 +213,11 @@ const Button = styled.div`
   &:hover {
     box-shadow: 0 3px 15px rgba(94, 129, 244, 0.1);
     transform: translateY(-2px);
+  }
+
+  @media (max-width: 375px) {
+    width: 158px;
+    height: 46px;
   }
 `;
 
@@ -143,10 +240,22 @@ const SocialLogin = styled.div`
         border-color: ${colors.primary};
       }
     }
+  }
+  .siginText {
+    color: ${colors.primaryLight};
+    margin-left: 13px;
+  }
+
+  @media (max-width: 375px) {
+    display: grid;
+    grid-template-areas: 1fr 1fr;
+    place-items: center;
+    width: 100%;
 
     .siginText {
-      color: ${colors.primaryLight};
-      margin-left: 13px;
+      grid-row: 1 / 2;
+      /* color: ${colors.primaryLight}; */
+      /* margin-left: 13px; */
     }
   }
 `;
@@ -163,4 +272,6 @@ export {
   BottomFormGroup,
   Button,
   SocialLogin,
+  SmallScreenTopTitles,
+  FormWrapper,
 };
